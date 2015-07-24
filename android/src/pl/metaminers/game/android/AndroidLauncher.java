a9e5a76dfb3e7861cc2f0a401ab5b9a6e715c720
tmachows <tymo5@wp.pl>
2015-07-24 20:31:55 +0200

    first commit

---
+++ android/src/pl/metaminers/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.metaminers.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.metaminers.game.MineFooGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MineFooGame(), config);
+	}
+}
---
+++ desktop/src/pl/metaminers/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.metaminers.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.metaminers.game.MineFooGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MineFooGame(), config);
+	}
+}
